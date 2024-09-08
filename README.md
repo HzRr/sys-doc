b972994d1c4fed4e97074747af739154fe719664
hzrr <origami_suki@outlook.com>
2024-09-08 11:06:18 +0800

    initialize vuepress

---
+++ README.md
@@ -0,0 +1,1 @@
+# ISCXDU-SYS-DOC
---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,21 @@
+import * as clientConfig0 from '/home/origami/Projects/iscxdu-sys-doc/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.42_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@_a35ujvne2yigddna3iov27fqja/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from '/home/origami/Projects/iscxdu-sys-doc/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.42_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc_zzyti2lohysypyilbblzu6yaw4/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from '/home/origami/Projects/iscxdu-sys-doc/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.42_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc.7_ekdqoluscttlu4b4k4vgsocgda/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from '/home/origami/Projects/iscxdu-sys-doc/node_modules/.pnpm/@vuepress+plugin-markdown-hint@2.0.0-rc.42_markdown-it@14.1.0_vuepress@2.0.0-rc.15_@vuepress+_nii32cgdfccwwrvgjnic5ynraa/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from '/home/origami/Projects/iscxdu-sys-doc/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.42_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc_mo6vhav3k3ghot22tiswmkcugi/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig5 from '/home/origami/Projects/iscxdu-sys-doc/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.42_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc.7_f3kgorxvl62iohmg5mjsntyu2i/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig6 from '/home/origami/Projects/iscxdu-sys-doc/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig7 from '/home/origami/Projects/iscxdu-sys-doc/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.42_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc._3hps6hidm3342jaeib7e7w7e7m/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig8 from '/home/origami/Projects/iscxdu-sys-doc/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.42_markdown-it@14.1.0_vuepress@2.0.0-rc.15_@vuepress+bundler_bsdrfhqs4iinfoqepljw6txoda/node_modules/@vuepress/theme-default/lib/client/config.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+].map((m) => m.default).filter(Boolean)
